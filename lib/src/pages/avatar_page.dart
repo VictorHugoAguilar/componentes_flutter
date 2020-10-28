5bf3fca85e68476222c887e888619731aca03215
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-10-28 22:01:09 +0100

    Creacion componente para poner un avatar, distinto tipo con imagen o sin imagen

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,32 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
-        actions: <Widget>[],
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(2.0),
+            margin: EdgeInsets.only(right: 5),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.famousbirthdays.com/faces/lee-stan-image.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.purple,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://www.famousbirthdays.com/faces/lee-stan-image.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
