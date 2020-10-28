5e094a9e1d76473f06a8eb4b7ebb90a2613b4d77
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-10-28 21:50:11 +0100

    Componente de alert, creacion y personalizacion

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,15 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Mostar alerta"),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlerta(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
@@ -15,4 +24,39 @@ class AlertPage extends StatelessWidget {
       ),
     );
   }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Contenido'),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () => {
+                        Navigator.of(context).pop(),
+                      },
+                  child: Text('Cancelar')),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Ok'))
+            ],
+          );
+        });
+  }
+  // Final AlertPage
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,7 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[],
       ),
     );
   }
